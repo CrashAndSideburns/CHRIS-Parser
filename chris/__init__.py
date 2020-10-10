5e449b778bf40b223927701b890384e736c3e909
CrashAndSideburns <62062835+CrashAndSideburns@users.noreply.github.com>
2020-10-10 01:43:24 -0700

    what
    issues galore, i have no idea how to fix it. i'll figure it out tomorrow

--- chris/__init__.py
+++ chris/__init__.py
@@ -1,1 +1,1 @@
-
+import chris
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chris-parser",
-    version="1",
+    version="1.4",
     author="Morgan Arnold",
     author_email="morgan.r.arnold@outlook.com",
     description="A parser for the .chris file type.",
